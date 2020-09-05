eb1ae309f9d9b774b7de3e0c51b40f7f468e4887
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2020-09-05 12:14:02 -0300

    Add CropImage Controller

---
+++ IsItReal/Scenes/Controller/CropImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CropImageViewController.swift
+//  IsItReal
+//
+//  Created by Vinicius Mesquita on 05/09/20.
+//  Copyright © 2020 Vinicius Mesquita. All rights reserved.
+//
+
+import UIKit
+
+class CropImageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
